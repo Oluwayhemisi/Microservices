c98ea2ebaa87f10d66af56b28690b85b09c5d77e
oluwayhemisi <ismailoluwayemisi@gmail.com>
2023-09-12 22:16:34 +0100

    "Implemented the Eureka server and register other services"

--- employee-microservices/src/main/java/com/interswitch/employeemicroservices/client/ApiClient.java
+++ employee-microservices/src/main/java/com/interswitch/employeemicroservices/client/ApiClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8484", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface ApiClient {
 
     @GetMapping("api/v1/department/code/{code}")
