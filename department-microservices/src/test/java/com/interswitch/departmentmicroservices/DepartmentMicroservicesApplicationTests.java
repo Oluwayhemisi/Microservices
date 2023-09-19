1e28208b53a60585944de20682769ac162c3e705
oluwayhemisi <ismailoluwayemisi@gmail.com>
2023-09-19 12:13:08 +0100

    "Updated files"

--- department-microservices/src/test/java/com/interswitch/departmentmicroservices/DepartmentMicroservicesApplicationTests.java
+++ department-microservices/src/test/java/com/interswitch/departmentmicroservices/DepartmentMicroservicesApplicationTests.java
@@ -3,11 +3,11 @@ package com.interswitch.departmentmicroservices;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DepartmentMicroservicesApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class DepartmentMicroservicesApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
